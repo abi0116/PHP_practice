43c33c947a0cea6e6dc4f88d516d34f8aa689cd7
abi0116 <77713619+abi0116@users.noreply.github.com>
2021-06-12 15:35:32 +0900

    progateⅣ

--- menu.php
+++ menu.php
@@ -16,34 +16,3 @@ class Menu {
   public function hello() {
     echo '私は'.$this->name.'です';
   }
-  
-  public function getName() {
-    return $this->name;
-  }
-  
-  public function getImage() {
-    return $this->image;
-  }
-  
-  public function getOrderCount() {
-    return $this->orderCount;
-  }
-  
-  public function setOrderCount($orderCount) {
-    $this->orderCount = $orderCount;
-  }
-  
-  public function getTaxIncludedPrice() {
-    return floor($this->price * 1.08);
-  }
-  
-  public function getTotalPrice() {
-    return $this->getTaxIncludedPrice() * $this->orderCount;
-  }
-  
-  public static function getCount() {
-    return self::$count;
-  }
-  
-}
-?>
