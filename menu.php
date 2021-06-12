ff6cdeb254f8db9f5f2e49a3fea1f95bf2fa3125
abi0116 <77713619+abi0116@users.noreply.github.com>
2021-06-12 16:44:44 +0900

    [fix]menu.php

--- menu.php
+++ menu.php
@@ -16,3 +16,53 @@ class Menu {
   public function hello() {
     echo '私は'.$this->name.'です';
   }
+  
+  public function getName() {
+    return $this->name;
+  }
+  
+  public function getImage() {
+    return $this->image;
+  }
+  
+  public function getOrderCount() {
+    return $this->orderCount;
+  }
+  
+  public function setOrderCount($orderCount) {
+    $this->orderCount = $orderCount;
+  }
+  
+  public function getTaxIncludedPrice() {
+    return floor($this->price * 1.08);
+  }
+  
+  public function getTotalPrice() {
+    return $this->getTaxIncludedPrice() * $this->orderCount;
+  }
+  
+  public function getReviews($reviews) {
+    $reviewsForMenu = array();
+    foreach ($reviews as $review) {
+      if ($review->getMenuName() == $this->name) {
+        $reviewsForMenu[] = $review;
+      }
+    }
+    return $reviewsForMenu;
+  }
+  
+  
+  public static function getCount() {
+    return self::$count;
+  }
+  
+  public static function findByName($menus, $name) {
+    foreach ($menus as $menu) {
+      if ($menu->getName() == $name) {
+        return $menu;
+      }
+    }
+  }
+  
+}
+?>
